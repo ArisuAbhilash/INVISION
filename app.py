70c2fe49a349bef3586cecbe9ecd3d740db08193
ArisuAbhilash <mauryaabhilash912@gmail.com>
2025-09-06 00:45:54 +0530

    feat(home.html): seperated the base and home html file and added the bg picture

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"  # required for flash messages
 
 # Ensure folder for saving graphs exists
 if not os.path.exists("static/images"):
@@ -12,7 +13,10 @@ if not os.path.exists("static/images"):
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html", title="Home")
+
+
+
 
 
 if __name__ == "__main__":
